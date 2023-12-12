509a516fdc6e3b221227cb7a565c2bf255cb795f
Nikhil Thakur <nikhilthakur8012004@gmail.com>
2023-12-13 02:43:02 +0530

    Changed file name app to index

---
+++ index.js
@@ -0,0 +1,51 @@
+// Built in Moudule
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
+const PORT = 8000;
+const cookieParser = require("cookie-parser");
+
+// User-defined Module
+const staticRouter = require("./routes/staticRouter");
+const userRouter = require("./routes/user");
+const { connectToMongoose } = require("./connect");
+const authentication = require("./middleware/auth");
+const setUpSocketIO = require("./Web Socket/socket").default;
+const User = require("./models/user");
+const Chat = require("./models/chat");
+const apiRouter = require("./routes/api");
+
+//Database Connection
+connectToMongoose(
+  "mongodb+srv://nikhilthakur8012004:sxvrWiUVpW9kv1iY@cluster0.ngblgwi.mongodb.net/Chatify"
+);
+// App intialisation
+const app = express();
+const server = http.createServer(app);
+// const io = new Server(server);
+setUpSocketIO(server);
+
+// Middleware
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(authentication);
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+//Routing
+app.use("/", staticRouter);
+app.use("/user", userRouter);
+app.use("/api", apiRouter);
+
+const nikhil = express();
+nikhil.get("nikhil", (req, res) => {
+  res.json("hiii");
+});
+
+// Server listening
+server.listen(PORT, () =>
+  console.log(`Server Started Successfully at Port ${PORT}`)
+);
